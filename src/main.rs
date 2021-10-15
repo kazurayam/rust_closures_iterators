799778f84d6bb4806121c6c49746d8a09ae7657d
kazurayam <kazuaki.urayama@gmail.com>
2021-10-15 20:19:03 +0900

    Listing13-17

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 fn main() {
-    let v1 = vec![1, 2, 3];
-    let v1_iter = v1.iter();
-    for val in v1_iter {
-        println!("Got: {}", val);
-    }
+    let v1: Vec<i32> = vec![1, 2, 3];
+    v1.iter().map(|x| x + 1);
 }
