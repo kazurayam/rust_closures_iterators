20d14d0c8a84b65f6d1795cf55a82ba2c683b94a
kazurayam <kazuaki.urayama@gmail.com>
2021-10-15 16:17:32 +0900

    move

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let x = 4;
-    let equal_to_x = |z| z == x;
-    let y = 4;
+    let x = vec![1, 2, 3];
+
+    let equal_to_x = move |z| z == x;
+
+    //println!("can't use x here: {:?}", x);
+
+    let y = vec![1, 2, 3];
+
     assert!(equal_to_x(y));
 }
