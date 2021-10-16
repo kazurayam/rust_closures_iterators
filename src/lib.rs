0d50360e89e3b91b07ca999c30c34dcaba149a95
kazurayam <kazuaki.urayama@gmail.com>
2021-10-16 11:03:14 +0900

    Listing13-24

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,31 @@
-struct Counter {
-    count: u32,
+use std::error::Error;
+use std::fs;
+use std::env;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("contents: {}", contents);
+    Ok(())
 }
 
-impl Counter {
-    fn new() -> Counter {
-        Counter { count: 0 }
-    }
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
 }
 
-impl Iterator for Counter {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.count < 5 {
-            self.count += 1;
-            Some(self.count)
-        } else {
-            None
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
         }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
-}
-
-#[test]
-fn calling_next_directly() {
-    let mut counter = Counter::new();
-
-    assert_eq!(counter.next(), Some(1));
-    assert_eq!(counter.next(), Some(2));
-    assert_eq!(counter.next(), Some(3));
-    assert_eq!(counter.next(), Some(4));
-    assert_eq!(counter.next(), Some(5));
-    assert_eq!(counter.next(), None);
-
-}
-
-#[test]
-fn using_other_iterator_trait_methods() {
-    let sum: u32 = Counter::new()
-        .zip(Counter::new().skip(1))
-        .map(|(a, b)| a * b)
-        .filter(|x| x % 3 == 0)
-        .sum();
-    assert_eq!(18, sum);
 }
