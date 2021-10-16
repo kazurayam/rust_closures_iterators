a46fb88bfc66737498f5f453c0bd91a9b4d9cee2
kazurayam <kazuaki.urayama@gmail.com>
2021-10-16 12:12:15 +0900

    Listing13-29

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use std::process;
 use rust_closures_iterators::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
