e2b7168d65b3206ad38207a7d443969ee0572543
kazurayam <kazuaki.urayama@gmail.com>
2021-10-16 10:33:43 +0900

    Listing13-21

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,22 @@
-#[test]
-fn iterator_demonstration() {
-
-    let v1 = vec![1, 2, 3];
-
-    let mut v1_iter = v1.iter();
-
-    assert_eq!(v1_iter.next(), Some(&1));
-    assert_eq!(v1_iter.next(), Some(&2));
-    assert_eq!(v1_iter.next(), Some(&3));
-    assert_eq!(v1_iter.next(), None);
-
+struct Counter {
+    count: u32,
 }
 
-#[test]
-fn iterator_sum() {
-    let v1 = vec![1, 2, 3];
-    let v1_iter = v1.iter();
-    let total: i32 = v1_iter.sum();
-    assert_eq!(total, 6);
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
 }
